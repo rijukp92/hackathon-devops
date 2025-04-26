c3d94389f8be56e9ad555389bdddce15b1c891e5
rijukp92 <132587507+rijukp92@users.noreply.github.com>
2025-04-26 18:15:38 +0530

    Delete terraform/modules/loki directory

--- terraform/modules/loki/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "loki_data_bucket_name" {
-  description = "Loki data bucket name"
-  value       = aws_s3_bucket.loki-data.bucket
-}
-
-output "loki-irsa-role-arn" {
-  description = "IAM Role ARN for loki Service account"
-  value       = aws_iam_role.loki.arn 
-}
