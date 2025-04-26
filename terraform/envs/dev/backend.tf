d452a8c5b74d45b70bcc453794a39a77006363a2
rijukp92 <132587507+rijukp92@users.noreply.github.com>
2025-04-26 17:54:49 +0530

    Delete terraform/envs directory

--- terraform/envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "hackathon-devops-tfstate-bucket-101"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "hackathon-devops-terraform-locks"
-  }
-}
--- terraform/envs/staging/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "hackathon-tfstate-bucket-101"
-    key            = "staging/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-  }
-}
